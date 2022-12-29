4a57f350168effced92cac5a74710340aab962c4
sidhu-033 <51311492+sidhu-033@users.noreply.github.com>
2022-12-29 12:44:51 +0530

    Update index.js

--- pc/index.js
+++ pc/index.js
@@ -2,27 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import Reduce from "./redux/Main"
-import store from "./addtocart/store"
-// import store1 from "./redux cart/store"
-// import Main from "./redux cart/Main"
-import {Provider} from "react-redux"
-import Calculator from './Calculator';
-import Travel from "./addtocart/travel"
-// import Set from "./Settimeout"
-// import Cart from "./redux cart/Cart"
-// import Check from "./check"
-// import Fetch from "./Fetch"
-// import Useref from "./Useref"
-// import Page1 from "./New folder/Page1"
-// import App from "./Add cart/App"
-// import A from "./child/A"
-import Signup from "./pc/signup"
-import Signin from "./pc/signin"
-import Home from "./pc/home"
+
+
 import Route from "./pc/routes"
 
-// import Todo from "./Todo"
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -30,13 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-     <Provider store={store}>
       <BrowserRouter>
-        {/* <Route/> */}
-        <Travel/>
-        {/* <Calculator/> */}
-        </BrowserRouter>
-    </Provider>
+        <Route/> 
+      </BrowserRouter>
 
   </React.StrictMode>
 );
